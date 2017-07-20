e376ef765380cb5c005618f93ffbd66dc4f2e403
Admin <Admin@Runner-PC>
2017-07-20 13:41:14 +0300

    add a+b

--- t3/src/tt3/Main.java
+++ t3/src/tt3/Main.java
@@ -3,8 +3,11 @@ package tt3;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int a=3;
+		int b=5;
+		
+		System.out.println(a+b);
+		
 	}
 
 }
